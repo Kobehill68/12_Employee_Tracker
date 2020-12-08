93312a3c69f278b1ff2387ca55995a7baab897b2
KobeHill68 <kobehill68@gmail.com>
2020-12-08 14:41:49 -0600

    fixed some errors

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
