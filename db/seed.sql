5d584b41f69115c675bdc7d21007c47b1388d0f6
KobeHill68 <kobehill68@gmail.com>
2020-12-08 13:39:54 -0600

    done with seeding the datebase

--- db/seed.sql
+++ db/seed.sql
@@ -1,34 +1,10 @@
 USE employee_trackerDB
 
-INSERT INTO department(title)
-VALUES
-    ('Management'),
-    ('Sales'),
-    ('Editing'),
-    ('Accounting'),
-    ('Branding'),
-    ('Recording');
-    
-INSERT INTO role(title, salary, department_id)
-VALUES
-	('Regional Manager', 500000, 1),
-    ('Sales Rep', 70000, 2),
-    ('Brand Rep', 90000, 3),
-    ('Editer', 50000, 4),
-    ('Accountant', 100000, 5),
-    ('Recording Rep', 120000, 6);
-    
-INSERT INTO employee(first_name, last_name, role_id)
-VALUES
-  ('Janet', 'Riggin', 1)
-, ('Tiffani', 'Kicksey', 6)
-, ('James', 'Jameson', 4)
-, ('Toni', 'Reeves', 5)
-, ('Toby', 'Majors', 3)
-, ('Derrick', 'Thomas', 2);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Kobe', 'Hill', 1);
 
-UPDATE `employee_trackerDB`.`employee` SET `manager_id` = '1' WHERE (`id` > '1');
-SELECT * FROM employee;
-SELECT * FROM role;
-SELECT * FROM department
+INSERT INTO role (title, salary, department_id)
+VALUES ('Manager', '1000', 1);
 
+INSERT INTO department (name)
+VALUES ('CEO');
