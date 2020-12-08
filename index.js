bcc01f7ad574f54a8d22cc0f99953937c7a2c0b0
KobeHill68 <kobehill68@gmail.com>
2020-12-07 19:07:50 -0600

    changed my code

--- index.js
+++ index.js
@@ -1,120 +1,91 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql")
-const consTable = require("console.table")
-const connection = require("./db/connection");
+const consTable = require("console.table");
+const { start } = require("repl");
 
-const startScreen = ["View all Employees", "View all Employees by Department", "Add Employee", "Remove Employee", "Update Employee Role", "Add Department", "Add Role", "Exit"]
 
-const startApp = () => {
-    inquirer.prompt({
-        name: "selectMenu",
-        type: "list",
-        message: "Select an option",
-        choices: startScreen
-    }).then((answer) => {
-        switch (answer.selectMenu) {
-            case 'View all Employees':
-                showAllEmp();
-                break;
-            case 'View all Employees by Department':
-                showAllByDept();
-                break;
-            case 'Add Employee':
-                addEmployee();
-                break;
-            case 'Remove Employee':
-                removeEmployee();
-                break;
-            case 'Update Employee Role':
-                updateRole();
-                break;
-            case 'Add Department':
-                addDept();
-                break;
-            case 'Add Role':
-                addRole();
-                break;
-            case 'Exit':
-                connection.end();
-                break;
-        }
-    })
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employee_trackerDB"
+});
+  
+connection.connect(function(err) {
+    if (err) throw err;
+    start()
+});
 
-}
-
-const showAllEmp = () => {
-    connection.query("SELECT * FROM department, role, employee", (err, results) => {
-        console.log(results);
-        startApp()
-    })
-}
-
-const showAllByDept = () => {
-    connection.query("SELECT * FROM department", (err, results) => {
-        if (err) throw err;
-        console.log(results);
-        startApp();
-    })
-}
-
-const addEmployee = () => {
+function start() {
     inquirer.prompt([{
-        type: 'input',
-        message: "What's the employees first name?",
-        name: 'firstName',
-    },
-    {
-        type: 'input',
-        message: "What's the employees last name?",
-        name: 'lastName',
-    },
-    {
-        type: 'number',
-        message: "What's the employees role ID?",
-        name: 'roleID',
-    },
-    {
-        type: 'number',
-        message: "What's the employees manager's ID?",
-        name: 'managerID',
-    }
-    ]).then((res) => {
-        connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.roleID, res.managerID], function (err, results) {
-            if (err) throw err;
-            console.table("Successfully Inserted");
-        })
-    })
+
+    }])
 }
 
-const removeEmployee = () => {
-    inquirer.prompt([
-        {
-          type: "input",
-          message: "Which employee would you like to remove?",
-          name: "title"
-        }
-      ]).then((res) => {
-        connection.query('DELETE FROM employee WHERE (?);', [res.employee],
-          function (err, results) {
-            if (err) throw err;
-            console.log("Successfully Removed");
-            beginApp();
-          })
-      })
-}
-
-const updateRole = () => {
+function addToTable() {
 
 }
 
-const addDept = () => {
+function addDepartment() {
 
 }
 
-const addRole = () => {
+function addRole() {
+
+}
+
+function addEmployee() {
 
 }
 
 
+function viewTable() {
 
-startApp()
+}
+
+function viewDepartments() {
+
+}
+
+function viewRole() {
+
+}
+
+function viewEmployee() {
+
+}
+
+function removeEmployee() {
+
+}
+
+function updateEmployee() {
+    
+}
+
+function updateRole() {
+    
+}
+
+function updateManager() {
+    
+}
+
+function utilReturnChoice() {
+
+}
+
+function determineMananger() {
+
+}
+
+function getManagerId() {
+
+}
+
+function returnEmployee(element) {
+    return `ID:${element.Employee_ID.toString().padStart(3, 'O')}`
+}
+
+  
